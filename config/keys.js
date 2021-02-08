1e51f2d7936017b41f178068b280b42250f66de0
Dibyajyoti Parida <paridadibyajyoti4@gmail.com>
2021-02-08 11:37:26 +0530

    Update keys.js
    confidential removed

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    googleClientId: "192311858158-l1llfnoimq40ae7dkdogjj59ithmcnoi.apps.googleusercontent.com",
-    googleClientSecret: "DZCOcNf67I6VrmlZCj2jxyWH",
-    dicordClientId: "716150561111801918",
-    discordClientSecret: "7zVL4zdlhTsEK-naR-VqzA-XhG9yZK1R"
+    googleClientId: "<client_id>",
+    googleClientSecret: "<client_secret>",
+    dicordClientId: "<discord_id>",
+    discordClientSecret: "<discor_secret>"
 }
